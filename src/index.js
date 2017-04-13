833d786df506f4262f73630780982a75f7fb9e0d
jdepriest <jdepriest@users.noreply.github.com>
2017-04-13 12:32:22 +0100

    adding initial commit with beard me changes to sample code

--- src/index.js
+++ src/index.js
@@ -3,26 +3,40 @@ var Alexa = require('alexa-sdk');
 var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 
 var languageStrings = {
-    "en": {
+    "en-GB": {
         "translation": {
             "FACTS": [
-                "A year on Mercury is just 88 days long.",
-                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-                "On Mars, the Sun appears about half the size as it does on Earth.",
-                "Earth is the only planet not named after a god.",
-                "Jupiter has the shortest day of all the planets.",
-                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-                "The Sun contains 99.86% of the mass in the Solar System.",
-                "The Sun is an almost perfect sphere.",
-                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-                "The temperature inside the Sun can reach 15 million degrees Celsius.",
-                "The Moon is moving approximately 3.8 cm away from our planet every year."
+              "The Balbo is an extended version of the Van Dyke which wraps around the mouth, with the ends of the mustache flared out beyond the lines that connect to the chin.",
+              "The simple term beard is an umbrella term which can include any style of facial hair, although generally excluding a mustache by itself.",
+              "A full beard is one which shows full, unmodified growth on all available areas of the face and neck, including the mustache, chin, sideburns, and cheeks.",
+              "The Chin curtain is a full beard without a mustache or neck hair.",
+              "The Chinstrap beard has sideburns which are connected to each other by a narrow line of hair along the jaw, resembling a helmet strap harnessed to one's chin.",
+              "The Clean shaven facial hairstyle contains no facial hair at all other than eyebrows and short, neatly trimmed sideburns.",
+              "Friendly muttonchops are Muttonchops which are connected by a mustache.",
+              "The Fu Manchu is a thin, narrow, mustache that grows downward in two very long tendrils from the upper lip, with the tapered, pointed ends hanging past the jawline.",
+              "The Goat patch is facial hair growing only from the chin directly beneath the mouth.",
+              "A Goatee is a beard style incorporating hair on the chin but not the cheeks.",
+              "The Goatee and mustache is a goatee in which the mustache is allowed to connect to the hair on the chin.",
+              "The Handlebar mustache is a mustache which has its ends grown much longer and often flared out.",
+              "A Horseshoe mustache is a full mustache with ends that extend down in parallel straight lines beyond the upper lip and down to the jawline.",
+              "A mustache is defined as any facial hair grown specifically on the upper lip.",
+              "Mutton chops are a more elaborate growth of sideburns which also grow larger toward the chin, resembling a mutton chop",
+              "A Neckbeard is a beard which does not include any hair on the face, but includes the hair of the neck, or under the jaw, or both.",
+              "A Pencil mustache is a mustache which is very thin, usually just above the line of the upper lip.",
+              "The Shenandoah is a fuller version of the chin curtain in which only the mustache is shaved, allowing the hair on the neck to grow out.",
+              "Sideburns are the patch of hair in front of the ears which connects a beard to the hair of one's head.",
+              "Side-whiskers are related to sideburns and muttonchops, but considerably more extreme.",
+              "The Soul Patch is the patch of hair just below the lower lip, not including the hair over the chin.",
+              "Stubble is any length of hair which is long enough to be seen, but short enough to not fully cover the skin beneath.",
+              "A Toothbrush mustache is a narrow but tall mustache which generally does not extend beyond the sides of the nose, and extends the full height of the upper lip.",
+              "The Van Dyke beard is a type of goatee in which the chin hair is disconnected from the mustache hair.",
+              "The Walrus mustache is characterized by whiskers that are thick, bushy, and droop over the mouth.",
+              "The Zappa consists of a wide soul patch and full mustache that extends slightly downward past the corners of the mouth. Named for American musician Frank Zappa."
+
             ],
-            "SKILL_NAME" : "Space Facts",
+            "SKILL_NAME" : "British Beard Me",
             "GET_FACT_MESSAGE" : "Here's your fact: ",
-            "HELP_MESSAGE" : "You can say tell me a space fact, or, you can say exit... What can I help you with?",
+            "HELP_MESSAGE" : "You can say tell me a beard fact, or, you can say exit... What can I help you with?",
             "HELP_REPROMPT" : "What can I help you with?",
             "STOP_MESSAGE" : "Goodbye!"
         }
@@ -30,65 +44,39 @@ var languageStrings = {
     "en-US": {
         "translation": {
             "FACTS": [
-                "A year on Mercury is just 88 days long.",
-                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-                "On Mars, the Sun appears about half the size as it does on Earth.",
-                "Earth is the only planet not named after a god.",
-                "Jupiter has the shortest day of all the planets.",
-                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-                "The Sun contains 99.86% of the mass in the Solar System.",
-                "The Sun is an almost perfect sphere.",
-                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-                "The temperature inside the Sun can reach 15 million degrees Celsius.",
-                "The Moon is moving approximately 3.8 cm away from our planet every year."
+              "The Balbo is an extended version of the Van Dyke which wraps around the mouth, with the ends of the mustache flared out beyond the lines that connect to the chin.",
+              "The simple term beard is an umbrella term which can include any style of facial hair, although generally excluding a mustache by itself.",
+              "A full beard is one which shows full, unmodified growth on all available areas of the face and neck, including the mustache, chin, sideburns, and cheeks.",
+              "The Chin curtain is a full beard without a mustache or neck hair.",
+              "The Chinstrap beard has sideburns which are connected to each other by a narrow line of hair along the jaw, resembling a helmet strap harnessed to one's chin.",
+              "The Clean shaven facial hairstyle contains no facial hair at all other than eyebrows and short, neatly trimmed sideburns.",
+              "Friendly muttonchops are Muttonchops which are connected by a mustache.",
+              "The Fu Manchu is a thin, narrow, mustache that grows downward in two very long tendrils from the upper lip, with the tapered, pointed ends hanging past the jawline.",
+              "The Goat patch is facial hair growing only from the chin directly beneath the mouth.",
+              "A Goatee is a beard style incorporating hair on the chin but not the cheeks.",
+              "The Goatee and mustache is a goatee in which the mustache is allowed to connect to the hair on the chin.",
+              "The Handlebar mustache is a mustache which has its ends grown much longer and often flared out.",
+              "A Horseshoe mustache is a full mustache with ends that extend down in parallel straight lines beyond the upper lip and down to the jawline.",
+              "A mustache is defined as any facial hair grown specifically on the upper lip.",
+              "Mutton chops are a more elaborate growth of sideburns which also grow larger toward the chin, resembling a mutton chop",
+              "A Neckbeard is a beard which does not include any hair on the face, but includes the hair of the neck, or under the jaw, or both.",
+              "A Pencil mustache is a mustache which is very thin, usually just above the line of the upper lip.",
+              "The Shenandoah is a fuller version of the chin curtain in which only the mustache is shaved, allowing the hair on the neck to grow out.",
+              "Sideburns are the patch of hair in front of the ears which connects a beard to the hair of one's head.",
+              "Side-whiskers are related to sideburns and muttonchops, but considerably more extreme.",
+              "The Soul Patch is the patch of hair just below the lower lip, not including the hair over the chin.",
+              "Stubble is any length of hair which is long enough to be seen, but short enough to not fully cover the skin beneath.",
+              "A Toothbrush mustache is a narrow but tall mustache which generally does not extend beyond the sides of the nose, and extends the full height of the upper lip.",
+              "The Van Dyke beard is a type of goatee in which the chin hair is disconnected from the mustache hair.",
+              "The Walrus mustache is characterized by whiskers that are thick, bushy, and droop over the mouth.",
+              "The Zappa consists of a wide soul patch and full mustache that extends slightly downward past the corners of the mouth. Named for American musician Frank Zappa."
+
             ],
-            "SKILL_NAME" : "American Space Facts"
-        }
-    },
-    "en-GB": {
-        "translation": {
-            "FACTS": [
-                "A year on Mercury is just 88 days long.",
-                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.",
-                "On Mars, the Sun appears about half the size as it does on Earth.",
-                "Earth is the only planet not named after a god.",
-                "Jupiter has the shortest day of all the planets.",
-                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-                "The Sun contains 99.86% of the mass in the Solar System.",
-                "The Sun is an almost perfect sphere.",
-                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-                "The temperature inside the Sun can reach 15 million degrees Celsius.",
-                "The Moon is moving approximately 3.8 cm away from our planet every year."
-            ],
-            "SKILL_NAME" : "British Space Facts"
-        }
-    },
-    "de": {
-        "translation": {
-            "FACTS": [
-                "Ein Jahr dauert auf dem Merkur nur 88 Tage.",
-                "Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.",
-                "Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.",
-                "Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.",
-                "Die Erde ist der einzige Planet, der nicht nach einem Gott benannt ist.",
-                "Jupiter hat den kürzesten Tag aller Planeten.",
-                "Die Milchstraßengalaxis wird in etwa 5 Milliarden Jahren mit der Andromeda-Galaxis zusammenstoßen.",
-                "Die Sonne macht rund 99,86 % der Masse im Sonnensystem aus.",
-                "Die Sonne ist eine fast perfekte Kugel.",
-                "Eine Sonnenfinsternis kann alle ein bis zwei Jahre eintreten. Sie ist daher ein seltenes Ereignis.",
-                "Der Saturn strahlt zweieinhalb mal mehr Energie in den Weltraum aus als er von der Sonne erhält.",
-                "Die Temperatur in der Sonne kann 15 Millionen Grad Celsius erreichen.",
-                "Der Mond entfernt sich von unserem Planeten etwa 3,8 cm pro Jahr."
-            ],
-            "SKILL_NAME" : "Weltraumwissen auf Deutsch",
-            "GET_FACT_MESSAGE" : "Hier sind deine Fakten: ",
-            "HELP_MESSAGE" : "Du kannst sagen, „Nenne mir einen Fakt über den Weltraum“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?",
-            "HELP_REPROMPT" : "Wie kann ich dir helfen?",
-            "STOP_MESSAGE" : "Auf Wiedersehen!"
+            "SKILL_NAME" : "American Beard Me",
+            "GET_FACT_MESSAGE" : "Here's your fact: ",
+            "HELP_MESSAGE" : "You can say tell me a beard fact, or, you can say exit... What can I help you with?",
+            "HELP_REPROMPT" : "What can I help you with?",
+            "STOP_MESSAGE" : "Goodbye!"
         }
     }
 };
@@ -106,7 +94,7 @@ var handlers = {
     'LaunchRequest': function () {
         this.emit('GetFact');
     },
-    'GetNewFactIntent': function () {
+    'GetNewBeardFactIntent': function () {
         this.emit('GetFact');
     },
     'GetFact': function () {
